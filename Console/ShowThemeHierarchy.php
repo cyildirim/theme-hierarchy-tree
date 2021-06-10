f299f574b3042670c6e0d0dd15030d164f3635fd
Can YILDIRIM <mcanyildirim@gmail.com>
2021-06-11 00:22:17 +0100

    hierarchy tree is now recursive

--- Console/ShowThemeHierarchy.php
+++ Console/ShowThemeHierarchy.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace Fetchtex\ThemeHierarchyTree\Console;
 
-use PBergman\Console\Helper\TreeHelper;
 use Magento\Theme\Model\ResourceModel\Theme\Collection as ThemeCollection;
+use Magento\Theme\Model\ResourceModel\Theme\CollectionFactory as ThemeCollectionFactory;
 use Magento\Theme\Model\Theme;
+use PBergman\Console\Helper\TreeHelper;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -17,11 +18,11 @@ class ShowThemeHierarchy extends Command
     /**
      * @var ThemeCollection
      */
-    private $collection;
+    private $collectionFactory;
 
-    public function __construct(ThemeCollection $collection)
+    public function __construct(ThemeCollectionFactory $collectionFactory)
     {
-        $this->collection = $collection;
+        $this->collectionFactory = $collectionFactory;
         parent::__construct();
     }
 
@@ -34,27 +35,53 @@ class ShowThemeHierarchy extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $items = $this->collection->getItems();
+        /**
+         * @var ThemeCollection $parentThemes
+         */
+        $parentThemes = $this->collectionFactory->create();
+        $items = $parentThemes->getItemsByColumnValue('paren_id', null);
+
         $tree = new TreeHelper();
 
         /**
          * @var Theme $item
          */
         foreach ($items as $item) {
-            $node = new TreeHelper();
-            if ($item->getParentId()) {
-                $node->setTitle($item->getCode());
-
-                $tree->findNode($item->getParentTheme()->getCode())[0]
-                    ->addNode($node)
-                    ->end();
-            } else {
-                $node->setTitle($item->getCode());
-                $tree->addNode($node)->end();
-            }
+            $this->addNode($item, $tree);
         }
 
         $tree->end();
         $tree->printTree($output);
+    }
+
+    /**
+     * @param Theme $item
+     * @param TreeHelper $tree
+     */
+    private function addNode($item, &$tree)
+    {
+        $node = new TreeHelper();
+
+        if ($tree->findNode($item->getCode())) {
+            return;
+        }
+
+        if ($item->getParentTheme()) {
+            $node->setTitle($item->getCode());
+            $tree->findNode($item->getParentTheme()->getCode())[0]->addNode($node)->end();
+        } else {
+            $node->setTitle($item->getCode());
+            $tree->addNode($node)->end();
+        }
+
+        /**
+         * @var ThemeCollection $childThemes
+         */
+        $childThemes = $this->collectionFactory->create();
+        $items = $childThemes->getItemsByColumnValue('parent_id', $item->getId());
+
+        foreach ($items as $item) {
+            $this->addNode($item, $tree);
+        }
     }
 }
